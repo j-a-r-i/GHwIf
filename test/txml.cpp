532324b19ec4586ba147236a16c149a38f688fec
Jari Ojanen <jojanen@elisanet.fi>
2018-10-05 21:48:36 +0300

    test for simple xml parser

---
+++ test/txml.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "../xmlparsesimple.h"
+
+const char *STR1 = "<foo><bar one=\"1\" two=\"2\"/></foo>";
+
+TEST(Xml, Test1)
+{
+    XmlParseSimple parser;
+
+    parser.parse(STR1);
+
+    EXPECT_EQ( 10.0, 10.0);
+}
