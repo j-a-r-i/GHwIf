131e262b1c3490812a8af1240fa4ebc51efd2176
Jari Ojanen <jojanen@elisanet.fi>
2018-10-29 21:18:20 +0200

    Config get and set initial implementation

--- config.cpp
+++ config.cpp
@@ -1,18 +1,24 @@
 /******************************************************************************
  * Copyright (C) 2018 Jari Ojanen
  ******************************************************************************/
-#include "luascript.h"
 #include "config.h"
+#include <string.h>
 
 #define MAX_STRING 80
 
-char ConfigData[][MAX_STRING] = {
-    [CFG_FMI_API]     = { FMI_API },
-    [CFG_STRAVA_API]  = { STRAVA_API },
-    [CFG_SCHEME_INIT] = { "init.scm" },
-    [CFG_SERIAL_PORT] = { "/dev/ttyS0" },
+typedef struct {
+    char value[MAX_STRING];
+    bool priv;
+} configitem_t;
 
-    [CFG_MAX] = NULL
+
+configitem_t CfgData[] = {
+    [CFG_FMI_API]     = { FMI_API,      true },
+    [CFG_STRAVA_API]  = { STRAVA_API,   true },
+    [CFG_SCHEME_INIT] = { "init.scm",   false },
+    [CFG_SERIAL_PORT] = { "/dev/ttyS0", false },
+
+    [CFG_MAX]         = { "",         true }
 };
 
 
@@ -22,8 +28,10 @@ void Cfg::init()
 
 const char* Cfg::get(CfgItem item)
 {
+    return CfgData[item].value;
 }
 
 void Cfg::set(CfgItem item, const char* value)
 {
+    strcpy(CfgData[item].value, value);
 }
