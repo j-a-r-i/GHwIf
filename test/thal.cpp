4eb8aa42f92cb59065e0cdc52e4a970972d37391
Jari Ojanen <jojanen@elisanet.fi>
2018-10-29 21:19:51 +0200

    cmake: cross compile for RPi and disable lua scripting

--- test/thal.cpp
+++ test/thal.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <chrono>
 #include <thread>
+#include <stdio.h>
 extern "C" {
   #include "hal.h"
 }
@@ -16,13 +17,16 @@ TEST(Hal, Io1)
     io_init();
     io_mode(PIN_NRF24_CE, IO_MODE_OUTPUT);
     
-    for (int i=0; i<200; i++) {
+    for (int i=0; i<80; i++) {
 	io_set(PIN_NRF24_CE);
 	delay_sec();
 	
 	io_clear(PIN_NRF24_CE);
 	delay_sec();
+
+	printf(".");
     }
+    printf("\n");
 }
 
 
@@ -31,21 +35,27 @@ TEST(Hal, Io2)
     io_init();
     io_mode(PIN_NRF24_CSN, IO_MODE_OUTPUT);
 
-    for (int i=0; i<200; i++) {
+    for (int i=0; i<80; i++) {
 	io_set(PIN_NRF24_CSN);
 	delay_sec();
 
 	io_clear(PIN_NRF24_CSN);
-	delay_sec();	
+	delay_sec();
+
+	printf(".");
     }
+    printf("\n");
 }
 
 TEST(Hal, Spi1)
 {
     spi_init(0);
 
-    for (int i=0; i<200; i++) {
+    for (int i=0; i<80; i++) {
 	spi_write(0, 0xF0F0);
 	delay_sec();
+
+	printf(".");
     }
+    printf("\n");
 }
